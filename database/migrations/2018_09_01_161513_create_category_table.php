3860eef43fccd3a2bcba7ccc24ed2c578a895238
Johnnatan <johnnatan.vargas@totemtech.net>
2018-09-01 11:30:14 -0600

    Agregado de inserts de categoria para migracion

--- database/migrations/2018_09_01_161513_create_category_table.php
+++ database/migrations/2018_09_01_161513_create_category_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateCategoryTable extends Migration
 {
@@ -15,11 +16,42 @@ class CreateCategoryTable extends Migration
     {
         Schema::create('category', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nombre',100);
-            $table->boolean('descripcion',256)->nullable();
-            $table->boolean('estado')->default(1);
+            $table->string('name',100);
+            $table->boolean('description',256)->nullable();
+            $table->boolean('status')->default(1);
             $table->timestamps();
         });
+
+        // Insert some stuff
+    DB::table('category')->insert(
+        array(
+            'name' => 'Antiinfecciosos'
+        )
+    );
+    DB::table('category')->insert(
+        array(
+            'name' => 'Antídotos‎'
+        )
+    );
+
+    DB::table('category')->insert(
+        array(
+            'name' => 'Antianémicos‎'
+        )
+    );
+
+    DB::table('category')->insert(
+        array(
+            'name' => 'Anticoagulantes‎'
+        )
+    );
+
+    
+    DB::table('category')->insert(
+        array(
+            'name' => 'Antihemorrágicos'
+        )
+    );
     }
 
     /**
