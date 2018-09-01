6a147c4ebfbf4243d17f55e7baf98a63e2ea2bb0
Johnnatan <johnnatan.vargas@totemtech.net>
2018-09-01 09:58:40 -0600

    Estructura del proyecto de tienda de medicinas en repositorio de GitHub

---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.styles([
+    'resources/assets/plantilla/css/style.css'
+], 'public/css/plantilla.css')
+.scripts([
+    'resources/assets/plantilla/js/main.js'
+], 'public/js/plantilla.js');
+
+/*
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
+*/
